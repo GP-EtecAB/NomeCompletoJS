21f66b9e9cb0a0e28d04ecb38a13a57f094252d9
Gustavo Pregigueiro <142064802+GP-EtecAB@users.noreply.github.com>
2023-10-02 22:09:57 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+let PrimNome = prompt("Digite seu primeiro nome:")
+let SegNome = prompt("Digite seu segundo nome:")
+
+let NomeCompleto = PrimNome + " " + SegNome
+
+alert("Seu nome completo é: " + NomeCompleto)
+
+let NomeCatalogo = SegNome.toUpperCase() + ", " + PrimNome
+
+alert("Seu nome de catálogo é: " + NomeCatalogo)
